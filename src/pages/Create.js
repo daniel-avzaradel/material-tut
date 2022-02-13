facb576a02ca0f44d90f6c633f7cb7a53d2c3caa
Daniel Avzaradel <daniela@cgen.com>
2022-02-13 15:23:26 +0200

    finished with buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Typography, Button } from "@material-ui/core";
+import { Typography, Button, ButtonGroup, Container } from "@material-ui/core";
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         component="h2"
@@ -13,8 +13,29 @@ export default function Create() {
         Create a New Note
       </Typography>
       <Button variant="contained" disableElevation>
-        Button
+        Contained
       </Button>
-    </div>
+      <br />
+      <br />
+      <Button variant="outlined" disableElevation>
+        Outlined
+      </Button>
+      <br />
+      <br />
+      <Button
+        color="secondary"
+        variant="contained"
+        onClick={() => console.log("you clicked me")}
+      >
+        Submit
+      </Button>
+      <br />
+      <br />
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </Container>
   );
 }
