40725c3387c11923ddcedfd559b32aa6804780d9
Daniel Avzaradel <daniela@cgen.com>
2022-02-13 16:07:29 +0200

    added form, textFields, validation and submit

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,11 @@
-import React from "react";
-import { Typography, Button, ButtonGroup, Container } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Typography,
+  Button,
+  ButtonGroup,
+  Container,
+  TextField,
+} from "@material-ui/core";
 import AcUnitIcon from "@material-ui/icons/AcUnit";
 import SendIcon from "@material-ui/icons/Send";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
@@ -17,10 +23,24 @@ const useStyles = makeStyles({
     textDecoration: "underline",
     marginBottom: 20,
   },
+  field: {
+    marginTop: 20,
+    marginTop: 20,
+    display: "block",
+  },
 });
 
 export default function Create() {
   const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
   return (
     <Container>
       <Typography
@@ -68,6 +88,39 @@ export default function Create() {
       <AcUnitIcon fontSize="large" />
       <AcUnitIcon />
       <AcUnitIcon fontSize="small" />
+      <br />
+      <br />
+      <form action="" noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          variant="outlined"
+          label="Note Title"
+          color="secondary"
+          fullWidth
+          required
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          variant="outlined"
+          label="Details"
+          color="secondary"
+          fullWidth
+          multiline
+          rows={4}
+          required
+        />
+        <br />
+        <Button
+          color="secondary"
+          variant="contained"
+          type="Submit"
+          endIcon={<SendIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
