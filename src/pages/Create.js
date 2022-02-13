5a5bb53ea73183cf48a13bfdd19c5df6240a843c
Daniel Avzaradel <daniela@cgen.com>
2022-02-13 14:39:41 +0200

    adding and using buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Button } from "@material-ui/core";
 
 export default function Create() {
   return (
     <div>
-      <Typography variant="h1">Create a New Note</Typography>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New Note
+      </Typography>
+      <Button variant="contained" disableElevation>
+        Button
+      </Button>
     </div>
   );
 }
