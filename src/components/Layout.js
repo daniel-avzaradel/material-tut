8c891a019a78db941a2fcba4f87113672efb9cf7
Daniel Avzaradel <daniela@cgen.com>
2022-02-16 18:06:14 +0200

    list and listitems

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
-import { Drawer, Typography } from "@material-ui/core";
+import {
+  Drawer,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import { SubjectOutlined, AddCircleOutlineOutlined } from "@material-ui/icons";
 
 const drawerWidth = 240;
 
@@ -18,10 +27,28 @@ const useStyles = makeStyles({
   root: {
     display: "flex",
   },
+  active: {
+    background: "#e5e5e5",
+  },
 });
 
 function Layout({ children }) {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+  const menuItems = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Create Note",
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/create",
+    },
+  ];
+
   return (
     <div className={classes.root}>
       {/* App bar */}
@@ -38,6 +65,24 @@ function Layout({ children }) {
             Ninja Notes
           </Typography>
         </div>
+
+        <List>
+          {menuItems.map((item) => {
+            return (
+              <ListItem
+                button
+                key={item.text}
+                onClick={() => history.push(item.path)}
+                className={
+                  location.pathname === item.path ? classes.active : null
+                }
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            );
+          })}
+        </List>
       </Drawer>
       <div className={classes.page}>
         <div>{children}</div>
