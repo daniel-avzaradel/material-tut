169f9cc4c53e855ff9cff748a529213f16279125
Daniel Avzaradel <daniela@cgen.com>
2022-02-16 16:48:14 +0200

    finished permanent drawer component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,47 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
+import { Drawer, Typography } from "@material-ui/core";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     background: "#f9f9f9",
     width: "100%",
   },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
+  },
 });
 
 function Layout({ children }) {
   const classes = useStyles();
   return (
-    <div className={classes.page}>
-      <div>{children}</div>
+    <div className={classes.root}>
+      {/* App bar */}
+
+      {/* Side Drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>
+            Ninja Notes
+          </Typography>
+        </div>
+      </Drawer>
+      <div className={classes.page}>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
