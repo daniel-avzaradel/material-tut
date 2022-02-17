9fbc731c30d8a17ee96fde882ab585185e30e402
Daniel Avzaradel <daniela@cgen.com>
2022-02-17 14:17:19 +0200

    finished avatar and MUI colors

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Avatar,
   Card,
   CardContent,
   CardHeader,
@@ -8,13 +9,21 @@ import {
 } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/styles";
+import { blue, green, pink, yellow } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
-  test: {
-    border: (note) => {
+  avatar: {
+    backgroundColor: (note) => {
       if (note.category === "work") {
-        return "1px solid red";
+        return yellow[700];
       }
+      if (note.category === "money") {
+        return green[500];
+      }
+      if (note.category === "todos") {
+        return pink[500];
+      }
+      return blue[500];
     },
   },
 });
@@ -25,6 +34,11 @@ function NoteCard({ note, handleDelete }) {
   return (
     <Card elevation={1} className={classes.test}>
       <CardHeader
+        avatar={
+          <Avatar className={classes.avatar}>
+            {note.category[0].toUpperCase()}
+          </Avatar>
+        }
         action={
           <IconButton onClick={() => handleDelete(note.id)}>
             <DeleteOutlined />
